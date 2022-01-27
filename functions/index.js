e924ece52ecf2622fca2e27dd4e70282f4658159
tbrew1023 <tbrew1023@gmail.com>
2022-01-27 01:52:37 -0800

    "openai setup"

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,29 @@
 const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+//// SDK Config ////
+
+const { Configuration, OpenAIApi } = require('openai');
+
+const configuration = new Configuration({
+    organization: functions.config().openai.id,
+    apiKey: functions.config().openai.key,
+});
+
+const openai = new OpenAIApi(configuration);
+
+exports.helloWorld = functions.https.onRequest(async (request, response) => {
+
+    const prompt = `Mars is `;
+
+    const gptCompletion = await openai.createCompletion('text-davinci-001', {
+        prompt: prompt,
+        temperature: 0.7,
+        max_tokens: 120,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+    });
+
+    response.send(gptCompletion.data.choices[0].text);
+
+});
